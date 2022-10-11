2344f0ecaf142bb34f2bba7e20b1544b01efc173
judyomocha <112790395+judyomocha@users.noreply.github.com>
2022-10-11 18:26:14 +0900

    Update app.py

--- app.py
+++ app.py
@@ -38,6 +38,30 @@ load_dotenv()
 TOKEN = os.environ['TOKEN']
 GOOGLE_CLOUD_PROJECT = os.environ['GOOGLE_CLOUD_PROJECT']
 
+
+@app.route("/")
+def hello() -> str:
+    # Use basic logging with custom fields
+    logger.info(logField="custom-entry", arbitraryField="custom-entry")
+    # https://cloud.google.com/run/docs/logging#correlate-logs
+    logger.info("Child logger with trace Id.")
+    return "Hello, World!"
+def shutdown_handler(signal_int: int, frame: FrameType) -> None:
+    logger.info(f"Caught Signal {signal.strsignal(signal_int)}")
+    from utils.logging import flush
+    flush()
+    # Safely exit program
+    sys.exit(0)
+if __name__ == "__main__":
+    # Running application locally, outside of a Google Cloud Environment
+    # handles Ctrl-C termination
+    signal.signal(signal.SIGINT, shutdown_handler)
+    app.run(host="localhost", port=8080, debug=True)
+else:
+    # handles Cloud Run container termination
+    signal.signal(signal.SIGTERM, shutdown_handler)
+
+
 intents: Intents = discord.Intents.all()
 client = discord.Client(intents=intents)
 
